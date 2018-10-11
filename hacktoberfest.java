5114d1065c4facaeff1ac97c2d991c1b9eb49ff5
davissmith1 <44070645+davissmith1@users.noreply.github.com>
2018-10-11 14:52:37 -0500

    Update hacktoberfest.java

--- hacktoberfest.java
+++ hacktoberfest.java
@@ -3,6 +3,7 @@ public class hactoberfest {
       System.out.println("Hello, my name is Hampton!");
       // add your name and print statement below
       System.out.println("Hey there, it's Mr. James!");
+      System.out.println("sup hamdog, im Davis");
    
    
    
